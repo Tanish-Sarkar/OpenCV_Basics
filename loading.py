f4284f215ba29517e3c852690f7325c4f535ed6d
Tanish-Sarkar <tanishnitch@gmail.com>
2025-09-15 19:20:04 +0530

    day2

--- loading.py
+++ loading.py
@@ -1,2 +1,17 @@
 import cv2
-image = cv2.imread("")
+
+# Loading the Image
+image = cv2.imread("./sample_images/00413-200124723.png")
+
+if image is None:
+    print("Error: image is empty")
+else:
+    print("Image Retrived Successfully")
+
+# Showing the image
+if image is not None:
+    cv2.imshow("Cat Image", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+else:
+    print("Error: image is empty")
