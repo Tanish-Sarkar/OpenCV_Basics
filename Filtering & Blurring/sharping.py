8605db305ad48757d172a8a1b3d9e62a63c654f5
Tanish-Sarkar <tanishnitch@gmail.com>
2025-10-14 12:34:48 +0530

    sharping

---
+++ Filtering & Blurring/sharping.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+image = cv2.imread("sample_images/00420-200124730.png")
+
+sharpen_kernal = np.array([
+    [0,-1,0],
+    [-1,5,-1],
+    [0,-1,0]
+])
+
+# cv2.filter2D(image, depth, sharpen_kernal)
+sharped = cv2.filter2D(image, -1, sharpen_kernal)
+
+cv2.imshow("Original Image", image)
+cv2.imshow("Shappened Image", sharped)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
