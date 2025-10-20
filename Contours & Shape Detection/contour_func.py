5a17fc1128043e5f7f46506c1e28ca2ed1ed3ed1
Tanish-Sarkar <tanishnitch@gmail.com>
2025-10-20 16:49:53 +0530

    shape detection

--- Contours & Shape Detection/contour_func.py
+++ Contours & Shape Detection/contour_func.py
@@ -1,7 +1,8 @@
 import cv2
 import sys
+import numpy as np
 
-absolute_path = "OpenCV_Basics\Contours & Shape Detection\triangle.jpg"
+absolute_path = "OpenCV_Basics/Contours & Shape Detection/triangle.jpg"
 image = cv2.imread(absolute_path) 
 
 # CRITICAL CHECK: Always verify if the image loaded successfully
@@ -20,7 +21,23 @@ contours, heirarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_S
 cv2.drawContours(image, contours, -1, (0,255,0), 2)
 
 for contour in contours:
-    
+    approx = cv2.approxPolyDP(contour, 0.01*cv2.arcLength(contour))
+
+    corner = len(approx)
+
+    if corner == 3:
+        shape_name = "Triangle"
+    elif corner == 4:
+        shape_name = "Rectangle"
+    else:
+        shape_name = "Polygon"
+
+    cv2.drawContours(image, [approx], 0, (0,255,0), 2)
+    x = approx.ravel()[0]
+    y = approx.ravel()[1] - 10
+    cv2.putText(image, shape_name, (x,y) ,cv2.FONT_HERSHEY_SIMPLEX, 0.6, 2)
+
+
 
 cv2.imshow("Contoured image", image)
 cv2.waitKey(0)
